39ce40e1bccdae898f7148286691d776795e30fd
svenkatramaninfoblox <svenkatraman@infoblox.com>
2026-05-14 12:56:24 -0600

    A few improvements

--- colo/docker/compressor.sh
+++ colo/docker/compressor.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-if [ -s $1 ]
+if [ -s "$1" ]
 then
   # The original filename is 127-0-0-1-8430.20200611.1958.1591905534.162597591.nmsg
   # We need to swap the time part to the front so that 'ls' can return sorted by time during uploading time
-  NEW_FILENAME=$(basename $1|awk -F'.' '{print $2"."$3"."$4"."$5"."$1".nmsg.zst"}')
+  OLD_FILENAME=$(basename "$1")
+  IFS='.' read -r field1 field2 field3 field4 field5 _ <<< "$OLD_FILENAME"
+  NEW_FILENAME="${field2}.${field3}.${field4}.${field5}.${field1}.nmsg.zst"
 
-  # Compress the file
-  zstd --rm -q -z $1 -o ${ZST_PARTIAL_DIR}/${NEW_FILENAME}
+  # Compress the file; on failure, clean up the partial output and the source
+  if ! zstd --rm -q -z "$1" -o "${ZST_PARTIAL_DIR}/${NEW_FILENAME}"; then
+    rm -f "${ZST_PARTIAL_DIR}/${NEW_FILENAME}"
+    rm -f "$1"
+    exit 1
+  fi
 
   # Move it to the upload queue when it is ready
-  mv ${ZST_PARTIAL_DIR}/${NEW_FILENAME} ${ZST_COMPLETE_DIR}/${NEW_FILENAME}
+  mv "${ZST_PARTIAL_DIR}/${NEW_FILENAME}" "${ZST_COMPLETE_DIR}/${NEW_FILENAME}"
 else
-  rm -f $1
+  rm -f "$1"
 fi
--- colo/docker/entrypoint.sh
+++ colo/docker/entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [[ "$POD_TYPE" == "COMPRESSOR" ]];
 then
-  echo "RUNNING $POD_TYPE `date +%s`"
+  echo "RUNNING $POD_TYPE $(date +%s)"
   mkdir -p ${NMSG_PARTIAL_DIR}
   mkdir -p ${ZST_PARTIAL_DIR}
   mkdir -p ${ZST_COMPLETE_DIR}
@@ -21,16 +21,17 @@ fi
 
 if [[ "$POD_TYPE" == "UPLOADER" ]];
 then
-  echo "RUNNING $POD_TYPE `date +%s`"
+  echo "RUNNING $POD_TYPE $(date +%s)"
   mkdir -p ${ZST_COMPLETE_DIR}
 
   while true
   do
-    # only list files end with zst
-    # if there is no file, output to /dev/null
-    ls ${ZST_COMPLETE_DIR}/*zst 2> /dev/null | xargs -n 1 -P ${UPLOADER_PARALLELISM} bash /bin/uploader.sh
+    # only process files ending with .zst; use find for robustness and null-safe handling
+    file_count=$(find "${ZST_COMPLETE_DIR}" -maxdepth 1 -name '*.zst' -print0 | xargs -0 -n 1 -P ${UPLOADER_PARALLELISM} bash /bin/uploader.sh 2>/dev/null | wc -l)
 
-    # short time to wait for more files
-    sleep 1
+    # if no files were found, sleep longer to reduce busy-looping
+    if [[ $file_count -eq 0 ]]; then
+      sleep 5
+    fi
   done
 fi
